d1ac6f6b7513c07a6c60d8e426d36b09e2df13c6
Matthew Akuhata <matthewakuhata@gmail.com>
2023-01-25 21:31:20 +1100

    Add job company association

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,6 +1,11 @@
-import { Job } from './db.js'
+import { Job, Company } from './db.js'
+
 export const resolvers = {
    Query: {
       jobs: () => Job.findAll(),
    },
+
+   Job: {
+      company: (root, ctx) => Company.findById(root.companyId)
+   }
 }
